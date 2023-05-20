0bbb946ef67d41b50ddfcb786d9547654dd4dc45
Anneli Hrginen <anneli.harginen@tktk.ee>
2023-05-20 10:23:34 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿// AND - && 
+
+// true && true $$ true --> true
+// true && false --> false
+// false && true --> false
+// false && false --> false
+
+
+// or - || (pipes)
+
+// true || true --> true
+//ture || false --> true
+//false || true --> ture
+// false || false --> false
